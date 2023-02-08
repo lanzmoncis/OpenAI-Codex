7b90da4df2a8a5f79740f99cd1bdbf837832d35c
lanzmoncis <lanzmoncis@gmail.com>
2023-02-08 15:03:08 +0000

    Update the server url

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch("http://localhost:5345", {
+  const response = await fetch("https://codex-i5ts.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application.json",
